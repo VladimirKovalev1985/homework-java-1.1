8443cf8687a9aec7c1969c97dc9b3d9c2d6caf04
Vladimir Kovalev <boban_envorter@mail.ru>
2021-09-29 20:06:04 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int current = 2_000_000_000;
+        int transfer = 500_000;
+        int total = current * transfer;
+        System.out.println(total);
+
+
+    }
+}
